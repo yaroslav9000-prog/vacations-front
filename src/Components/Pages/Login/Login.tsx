5eecac498cf69b9245b346a6620b0cfb66162a85
Yaroslav Yarkevich <yaroslavyarkevich@gmail.com>
2024-08-21 14:52:25 +0300

    made blueprints for login and register pages

--- src/Components/Pages/Login/Login.tsx
+++ src/Components/Pages/Login/Login.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./Login.css";
 
 function Login(): JSX.Element {
+    const [email, setEmail] = useState('');
+    const [pwd, setPwd] = useState('');
+    
     return (
         <div className="Login">
-			
+            <form action="">
+                <label htmlFor="email">Email</label>
+                <input type="text" id="email" name="email" onChange={(e)=>setEmail(e.target.value)}/>
+                <label htmlFor="">Password:</label>
+                <input type="text" id="pwd" name="pwd" onChange={(e)=>setPwd(e.target.value)}/>
+                <input type="submit" value={'Login'}/>
+            </form>
         </div>
     );
 }
--- src/Components/Pages/Register/Register.tsx
+++ src/Components/Pages/Register/Register.tsx
@@ -3,7 +3,17 @@ import "./Register.css";
 function Register(): JSX.Element {
     return (
         <div className="Register">
-			
+            <form action="">
+                <label htmlFor="">First Name</label>
+                <input type="text" />
+                <label htmlFor="">Last Name</label>
+                <input type="text" />
+                <label htmlFor="">Email</label>
+                <input type="text" />
+                <label htmlFor="">Password</label>
+                <input type="text" />
+                <input type="submit" value={"Register"}/>
+            </form>
         </div>
     );
 }
